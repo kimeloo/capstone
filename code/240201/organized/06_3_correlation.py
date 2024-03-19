e4d6775882d11fe1ad7adfa46d1cca9413b0625e
kimeloo <iroo200121@gmail.com>
2024-03-19 12:39:20 +0900

    ADD 240201/organized/06_3_correlation.py

--- code/240201/organized/06_3_correlation.py
+++ code/240201/organized/06_3_correlation.py
@@ -4,21 +4,16 @@ import matplotlib.pyplot as plt
 
 # 데이터 불러오기
 pwd = '~/Documents/Coding/capstone/data/240201'
-# filename = 'test_01_preprocess.csv'
 filename = '04_htnderv_s1_train.csv'
 data = pd.read_csv(f'{pwd}/{filename}')
 
-# Calculate the correlation matrix
+# 상관관계 계산
 corr_matrix = data.corr()
 top_corr_features = corr_matrix.index[abs(corr_matrix["htnderv_s1"])>=0.1]
 
-# Generate the heatmap
+# heatmap 생성
 plt.figure(figsize=(13,10))
-g = sns.heatmap(data[top_corr_features].corr(), annot=True, cmap="RdYlGn")
-# font size 조절
-plt.title('Correlation Matrix', fontsize=20)
-plt.xticks(fontsize=7)
-plt.yticks(fontsize=7)
-plt.tight_layout()
-# Save the heatmap
+g = sns.heatmap(data[top_corr_features].corr(), annot=True, annot_kws={"size": 5}, cmap="RdYlGn")
+pwd = '/Users/kimeloo'+pwd[1:]
+plt.savefig(f'{pwd}/06_3_correlation_heatmap.png')
 plt.show()
